4f09582154d241e44274073f62555fbe09863a99
hottabych <varvar1987a@mail.ru>
2024-05-30 20:02:44 +0300

    postgres

--- core/core/settings.py
+++ core/core/settings.py
@@ -140,10 +140,20 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'env(name_db)',
+        'USER': 'env(name_user)',
+        'PASSWORD': 'env(password)',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 # DATABASES = {
