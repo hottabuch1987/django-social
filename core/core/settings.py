fcf0eb2b4ef820b5ecbed9e670e44ead10c6d1c2
hottabych <varvar1987a@mail.ru>
2024-06-01 14:11:59 +0300

    +

--- core/core/settings.py
+++ core/core/settings.py
@@ -217,6 +217,14 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [ BASE_DIR / 'core' / 'static', ]
