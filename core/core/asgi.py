db93fc929aaf2a8c2722d342bcaf18f29f27ffe4
hottabych <varvar1987a@mail.ru>
2024-06-02 18:49:47 +0300

    daphne

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,3 +1,9 @@
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+import django
+django.setup()
+
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
