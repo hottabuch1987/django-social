f96d146452fc8d4dd2b88a84a15e4a5941e1a3a0
hottabych <varvar1987a@mail.ru>
2024-06-02 22:38:34 +0300

    admin-panel-for-user

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Forum, User, UserImage, Notification
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'birth_date',  'gender', "tel",
+                    'online_status', 'slug', 'date_joined')
+    list_filter = ('birth_date', 'username', 'first_name', 'slug')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'slug') 
+    ordering = ('date_joined',)
 
-admin.site.register(User)
+    def get_prepopulated_fields(self, request, obj=None):
+        return {
+            'slug': ('username',),
+        }
+
+    def get_readonly_fields(self, request, obj=None):
+        return ('slug','id',)
+
+
 
 admin.site.register(Forum)
 
