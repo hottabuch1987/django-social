050ebff289959dab40c72247294861c0b71e25c0
hottabych <varvar1987a@mail.ru>
2024-06-02 12:18:37 +0300

    7

--- core/core/settings.py
+++ core/core/settings.py
@@ -88,7 +88,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 16379)],
+            "hosts": [("localhost", 6379)],
            # "hosts": ["redis://core-redis:6379"],
         },
     },
