b5633a661f9515c475ead0b188e927c19feee3f0
hottabych <varvar1987a@mail.ru>
2024-06-07 17:23:19 +0300

    fix search exlude id

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,7 +22,7 @@ env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',  "195.133.32.53", "localhost",  "my-poster.ru", "www.my-poster.ru"]
 
@@ -142,25 +142,25 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('NAME_DB'),
-#         'USER': env('USER_NAME'),
-#         'PASSWORD': env('PASSWORD_DB'),
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('NAME_DB'),
+        'USER': env('USER_NAME'),
+        'PASSWORD': env('PASSWORD_DB'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 
